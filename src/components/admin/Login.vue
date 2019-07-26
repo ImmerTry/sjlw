b4bd8b2943f5730879c687b9518047aa5d6920f7
LIULEI <1136715789@qq.com>
2019-07-26 17:32:00 +0800

    modify Style

--- src/components/admin/Login.vue
+++ src/components/admin/Login.vue
@@ -1,25 +1,44 @@
 <template>
     <div class="login">
         <Row>
-            <Col :xs="12" :md="12" :lg="12" :xl="12">
-                <div class="left">
-                    <div class="text">
-                        <div class="item">
-                            <span class="iconfont">&#xe649;</span>
-                            Say Something
+            <Col :xs="24" :md="24" :lg="12" :xl="12">
+                <Row>
+                    <Col :xs="24" :md="{span: 12,offset:6}" :lg="0" :xl="0">
+                        <div class="content">
+                            <div class="form">
+                                <div class="logo">
+                                    <span class="Icon"></span>
+                                </div>
+                                <h1 class="tips">后台登录</h1>
+                                <Button class="register" type="primary" shape="circle" long>注册</Button>
+                                <br><br>
+                                <Button class="LoginBtn" type="primary" ghost shape="circle" long>登录</Button>
+                            </div>
                         </div>
-                        <div class="item">
-                            <span class="iconfont">&#xe615;</span>
-                            Say Something
+                    </Col>
+                </Row>
+                <Row>
+                    <Col :lg="24" :xl="24">
+                        <div class="left">
+                            <div class="text">
+                                <div class="item">
+                                    <span class="iconfont">&#xe649;</span>
+                                    Say Something
+                                </div>
+                                <div class="item">
+                                    <span class="iconfont">&#xe615;</span>
+                                    Say Something
+                                </div>
+                                <div class="item">
+                                    <span class="iconfont">&#xe61c;</span>
+                                    Say Something
+                                </div>
+                            </div>
                         </div>
-                        <div class="item">
-                            <span class="iconfont">&#xe61c;</span>
-                            Say Something
-                        </div>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </Col>
-            <Col :xs="12" :md="12" :lg="12" :xl="12">
+            <Col :xs="24" :md="24" :lg="{span:11,offset:1}" :xl="{span:9,offset:3}">
                 <div class="right">
                     <div class="LoginHeader">
                         <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
@@ -38,7 +57,24 @@
                             </FormItem>
                         </Form>
                     </div>
-                    <div class="content">
+                </div>
+            </Col>
+            <!-- 手机、pad -->
+            <Col :xs="24" :md="24" :lg="0" :xl="0">
+                <Row>
+                    <div class="btn-group">
+                        <Col :xs="{span:10,offset:1}" :md="{span:6,offset:6}" :lg="12" :xl="12" >
+                            <Button type="primary" shape="circle">注册</Button>
+                        </Col>
+                        <Col :xs="{span:10,offset:2}" :md="{span:6,offset:1}" :lg="12" :xl="12">
+                            <Button type="primary" ghost shape="circle">登录</Button>
+                        </Col>
+                    </div>
+                </Row>
+            </Col>
+            <Col :xs="0" :md="0" :lg="{span:8,offset:2}" :xl="{span:5,offset:3}">
+                <div class="content">
+                    <div class="form">
                         <div class="logo">
                             <span class="Icon"></span>
                         </div>
@@ -46,10 +82,17 @@
                         <Button class="register" type="primary" shape="circle" long>注册</Button>
                         <br><br>
                         <Button class="LoginBtn" type="primary" ghost shape="circle" long>登录</Button>
-                </div>
+                    </div>
                 </div>
             </Col>
         </Row>
+        <!-- <Row>
+            <Col>
+                <div class="footer">
+                    © 2019 ImmerTry, personal.
+                </div>
+            </Col>
+        </Row> -->
     </div>
 </template>
 <script>
@@ -95,24 +138,43 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-    .login {
+// xs
+@media only screen and (max-width: 600px) {
+    .content {
         width: 100%;
         height: 100%;
+        padding: 14px;
+        .tips {
+            width: 100%;
+            margin: 48px 0  16px 0;
+        }
+        .register {
+            padding: 6px 16px;
+        }
+        .LoginBtn {
+            padding: 6px 16px;
+        }
     }
     .left {
         width: 100%;
         height: 100%;
         background: #1da1f2;
         .text {
-            width: 231px;
+            width: 220px;
             height: 100%;
-            margin: 0 auto;
             color: #ffff;
+            padding: 14px 0;
+            margin: 0 auto;
             .item {
+                width: 100%;
                 display: block;
-                font-weight: bold;
-                line-height: 24px;
-                padding: 20px 0;
+                text-align: center;
+                font-size: 18px;
+                padding: 14px 0;
+                margin-bottom: 40px;
+                .iconfont {
+                    font-size: 21px;
+                }
             }
         }
     }
@@ -122,19 +184,28 @@ export default {
         margin: 0 auto;
         padding-top: 20px;
         .LoginHeader {
-            width: 472px;
-            margin: 0 auto;
-            padding: 0 20px;
+            display: none;
         }
-        .content {
-            max-width: 350px;
-            width: 243px;
-            height: 100%;
-            // margin: 0 auto;
-            position: relative;
-            // border: 1px solid red;
-            left: 25%;
-            top: 50%;
+    }
+    .btn-group {
+        width: 100%;
+        height: 100
+        px;
+        padding: 39px 15px 30px 15px;
+        text-align: center;
+        Button {
+            width: 100%;
+            // margin-right: 15px;
+        }
+    }
+}
+// md
+@media only screen and (min-width: 768px) {
+    .content {
+        width: 100%;
+        height: 100%;
+        padding: 14px;
+        .form {
             .tips {
                 width: 100%;
                 margin: 48px 0  16px 0;
@@ -147,4 +218,170 @@ export default {
             }
         }
     }
+    .left {
+        width: 100%;
+        height: 100%;
+        background: #1da1f2;
+        .text {
+            width: 220px;
+            height: 100%;
+            color: #ffff;
+            padding: 14px 0;
+            margin: 0 auto;
+            .item {
+                width: 100%;
+                display: block;
+                text-align: center;
+                font-size: 18px;
+                padding: 14px 0;
+                margin-bottom: 40px;
+                .iconfont {
+                    font-size: 21px;
+                }
+            }
+        }
+    }
+    .right {
+        width: 100%;
+        // height: 789px;
+        margin: 0 auto;
+        padding-top: 20px;
+        .LoginHeader {
+            display: none;
+        }
+    }
+    .btn-group {
+        width: 100%;
+        height: 100
+        px;
+        padding: 39px 15px 30px 15px;
+        text-align: center;
+        Button {
+            width: 100%;
+            // margin-right: 15px;
+        }
+    }
+}
+// lg
+@media only screen and (min-width: 992px) {
+    .content {
+        width: 100%;
+        height: 100%;
+        padding: 14px;
+        .form {
+            .tips {
+                width: 100%;
+                margin: 48px 0  16px 0;
+            }
+            .register {
+                padding: 6px 16px;
+            }
+            .LoginBtn {
+                padding: 6px 16px;
+            }
+        }
+    }
+    .left {
+        width: 100%;
+        height: 100vh;
+        background: #1da1f2;
+        .text {
+            width: 220px;
+            height: 100%;
+            color: #ffff;
+            padding: 14px 0;
+            margin: 0 auto;
+            .item {
+                width: 100%;
+                display: block;
+                text-align: center;
+                font-size: 18px;
+                padding: 14px 0;
+                margin-bottom: 40px;
+                .iconfont {
+                    font-size: 21px;
+                }
+            }
+        }
+    }
+    .right {
+        width: 100%;
+        // height: 789px;
+        padding-top: 20px;
+        .LoginHeader {
+            display: block;
+        }
+    }
+    .btn-group {
+        width: 100%;
+        height: 100
+        px;
+        padding: 39px 15px 30px 15px;
+        text-align: center;
+        Button {
+            width: 100%;
+            // margin-right: 15px;
+        }
+    }
+
+}
+// xl
+@media only screen and (min-width: 1200px) {
+    .content {
+        width: 100%;
+        height: 100%;
+        padding: 14px;
+        .form {
+            .tips {
+                width: 100%;
+                margin: 48px 0  16px 0;
+            }
+            .register {
+                padding: 6px 16px;
+            }
+            .LoginBtn {
+                padding: 6px 16px;
+            }
+        }
+    }
+    .left {
+        width: 100%;
+        height: 100vh;
+        background: #1da1f2;
+        .text {
+            width: 220px;
+            height: 100%;
+            color: #ffff;
+            padding: 14px 0;
+            margin: 0 auto;
+            .item {
+                width: 100%;
+                display: block;
+                text-align: center;
+                font-size: 18px;
+                padding: 14px 0;
+                margin-bottom: 40px;
+                .iconfont {
+                    font-size: 21px;
+                }
+            }
+        }
+    }
+    .right {
+        width: 100%;
+        padding-top: 20px;
+        .LoginHeader {
+            display: block;
+        }
+    }
+    .btn-group {
+        width: 100%;
+        height: 100px;
+        padding: 39px 15px 30px 15px;
+        text-align: center;
+        Button {
+            width: 100%;
+        }
+    }
+}
 </style>
